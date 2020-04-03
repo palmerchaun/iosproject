7cc6d2131ddb8e36d1480221bd33cef90a156b5d
abbyallen <ajallen10@gmail.com>
2020-04-03 15:12:44 -0600

    added core data

---
+++ Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Abby Allen on 4/3/20.
+//  Copyright © 2020 Seth Palmer. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
