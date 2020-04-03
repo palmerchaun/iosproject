ac7973e2501b47a78bc2eddb5144f57e0bfaf3c1
abbyallen <ajallen10@gmail.com>
2020-04-03 15:42:18 -0600

    added functional segue, waiting for scoring and end game to be developed

--- Game/GameViewController.swift
+++ Game/GameViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import CoreData
 
 class GameViewController: UIViewController {
 
--- Game/ViewController.swift
+++ Game/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
-
 }
